1c97c3594d8fb63356efb1b81cef98a7a54e6c99
Eng <>
2024-06-23 18:45:38 +0800

    change API to railway URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000', // FastAPI server URL
+  // baseURL: 'http://localhost:8000', // FastAPI server URL
+  baseURL: 'https://books-rec-be-production.up.railway.app', // FastAPI server URL
 });
 
 export default axiosInstance;
