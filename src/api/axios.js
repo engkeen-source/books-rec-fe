9792829e2a048d0f30c83def500638447c6fad49
Eng <>
2024-06-23 20:08:46 +0800

    updated backend URL in env

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: process.env.BACKEND_BASE_URL, // FastAPI server URL
-  baseURL: 'http://localhost:8000', // FastAPI server URL
+  baseURL: process.env.REACT_APP_BACKEND_BASE_URL, // FastAPI server URL
   });
 
 export default axiosInstance;
